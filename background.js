86a0c7f8b18b7864a2e5b472ba2f9bf4559d8905
andryou <andryou@gmail.com>
2013-09-30 13:31:06 -0400

    Initial Commit

---
+++ background.js
@@ -0,0 +1,1 @@
+chrome.webRequest.onBeforeRequest.addListener(function() { return { cancel: true }; }, {"urls": ["*://www.facebook.com/ajax/mercury/change_read_status.php", "*://www.facebook.com/ajax/messaging/typ.php"]}, ['blocking']);
